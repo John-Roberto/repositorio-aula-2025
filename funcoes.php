88e6069467f0eb884bd3ce5a141eb9efb6aecd25
John-Roberto <milesjohnprower92@gmail.com>
2025-04-17 10:35:06 -0300

    Adicionado um arquivo PHP com as seguintes funções: soma, subtracao, raizQuadrada

---
+++ funcoes.php
@@ -0,0 +1,13 @@
+<?php
+    function soma($num1, $num2): mixed{
+        return($num1 + $num2);
+    }
+
+    function subtracao($num1, $num2): mixed{
+        return($num1 - $num2);
+    }
+
+    function raizQuadrada(float $num){
+        return sqrt($num);
+    }
+?>
